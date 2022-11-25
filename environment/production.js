512a3d7bdabfff76e09d323b4aeafe3d4cebddbc
Stephanie-Bornarel <86592071+Stephanie-Bornarel@users.noreply.github.com>
2022-11-25 11:57:06 +0100

    correction prod.js

--- environment/production.js
+++ environment/production.js
@@ -2,5 +2,5 @@ module.exports = {
   dbUrl:
     "mongodb+srv://stef:qwe@cluster0.yckcaeb.mongodb.net/twitter?retryWrites=true",
   cert: "/etc/letsencrypt/live/dyma-projet.site/fullchain.pem",
-  key: " /etc/letsencrypt/live/dyma-projet.site/privkey.pem",
+  key: "/etc/letsencrypt/live/dyma-projet.site/privkey.pem",
 };
