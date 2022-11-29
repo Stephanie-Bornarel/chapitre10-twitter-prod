0145fd0aee7c0a9ae28afc6dc105bba91731eec7
Stephanie-Bornarel <stefbornarel@gmail.com>
2022-11-29 18:14:42 +0100

    maj prod.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "Twitter",
+      name: "chapitre10-twitter-prod",
       script: "./bin/www",
       instances: "max",
       autorestart: true,
@@ -12,24 +12,6 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       },
-    }
+    },
   ],
- 
-  // }, {
-  //   script: './service-worker/',
-  //   watch: ['./service-worker']
-  //
-
-  // deploy : {
-  //   production : {
-  //     user : 'SSH_USERNAME',
-  //     host : 'SSH_HOSTMACHINE',
-  //     ref  : 'origin/master',
-  //     repo : 'GIT_REPOSITORY',
-  //     path : 'DESTINATION_PATH',
-  //     'pre-deploy-local': '',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-  //     'pre-setup': ''
-  //   }
-  // }
 };
