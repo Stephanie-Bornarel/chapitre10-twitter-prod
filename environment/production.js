297f13326174c7300246683e5c6e2c1986b4e369
Stephanie-Bornarel <stefbornarel@gmail.com>
2022-11-29 18:34:06 +0100

    cert&key certbot

--- environment/production.js
+++ environment/production.js
@@ -1,8 +1,8 @@
 module.exports = {
   dbUrl:
     "mongodb+srv://stef:qwe@cluster0.yckcaeb.mongodb.net/twitter?retryWrites=true",
-  cert: "/etc/letsencrypt/live/dyma-projet.site/fullchain.pem",
-  key: "/etc/letsencrypt/live/dyma-projet.site/privkey.pem",
+  cert: "/etc/letsencrypt/live/www.dyma-projet.site/fullchain.pem",
+  key: "/etc/letsencrypt/live/www.dyma-projet.site/privkey.pem",
   portHttp: 80,
   portHttps: 443,
 };
