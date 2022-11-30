debfb6c5dfca2a5237e6d10ef12c2d76880e0467
Stephanie-Bornarel <stefbornarel@gmail.com>
2022-11-30 13:17:50 +0100

    test port

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ module.exports = {
   dbUrl: "mongodb+srv://stef:qwe@cluster0.yckcaeb.mongodb.net/twitter?retryWrites=true",
   cert: "/etc/letsencrypt/live/www.dyma-projet.site/fullchain.pem",
   key: "/etc/letsencrypt/live/www.dyma-projet.site/privkey.pem",
-  portHttp: 8000,
-  portHttps: 8443,
+  portHttp: 80,
+  portHttps: 443,
 };
